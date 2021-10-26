5956c4ead8ac5886dd4a82fe458384b0a8d8d3cb
11090112sS <kosukerino@gmail.com>
2021-10-26 22:24:23 +0900

    Add cooking_informationsにバリデーションの追加

--- app/models/cooking_information.rb
+++ app/models/cooking_information.rb
@@ -5,4 +5,7 @@ class CookingInformation < ApplicationRecord
   validates :cooking_id, uniqueness: { scope: [:fish_id, :handle_id] }
   validates :cooking_time, presence: true
   validates :cooking_url, presence: true
+  validates :after_handle_fish_processing_time, presence: true
+  validates :rest_fish_time, presence: true
+  validates :cooking_total_time, presence: true
 end
