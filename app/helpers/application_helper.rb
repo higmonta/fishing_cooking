2a201efebf1b6e2c609cc40fbc86078ae327cbd0
11090112sS <kosukerino@gmail.com>
2021-11-15 02:39:57 +0900

    Fix 料理検索機能のリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def cooking_image_path(fish_id:, cooking_id:)
     "cooking/#{Fish.find(fish_id).kind}/#{Cooking.find(cooking_id).name}.jpg"
   end
+
+  def operation_total_time(fish_id:, handle_id:, cooking_information:)
+    FishHandleInformation.find_by(fish_id: fish_id, handle_id: handle_id).handle_time + cooking_information.cooking_total_time
+  end
 end
