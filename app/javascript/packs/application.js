f31f042d2cfabbb4f7df3156e2ab20f860d0f92e
11090112sS <kosukerino@gmail.com>
2021-11-21 02:06:08 +0900

    Add セレクトボックスの要素を動的に出力する機能の追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,9 @@ require("channels")
 require('bootstrap')
 require('jquery')
 
-import "bootstrap";
+import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
-
+import '../javascripts/select_box';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
