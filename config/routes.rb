fb1bb5b0a17fb58f7dcd5f9d7e4d4b39c34a7d47
11090112sS <kosukerino@gmail.com>
2021-11-25 02:30:40 +0900

    Add 料理時間検索のルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  resources :cooking_informations, only: %i[index]
+  resources :cooking_informations, only: %i[index] do
+    get 'search_time', on: :collection
+  end
 end
