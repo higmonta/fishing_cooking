da5eaed1d9275f313f06c38c4d0834092b8ad28f
11090112sS <kosukerino@gmail.com>
2021-12-28 00:02:20 +0900

    POSTメソッドの料理時間検索のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   resources :cooking_informations, only: %i[index] do
     get 'search_time', on: :collection
+    post 'search_time', on: :collection
   end
 end
