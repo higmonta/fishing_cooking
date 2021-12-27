92dde6752190510c4a338052969afb56af39f9de
11090112sS <kosukerino@gmail.com>
2021-12-28 00:11:18 +0900

    料理時間検索の検索バーを追加

--- app/controllers/cooking_informations_controller.rb
+++ app/controllers/cooking_informations_controller.rb
@@ -5,4 +5,9 @@ class CookingInformationsController < ApplicationController
     @q = CookingInformation.ransack(params[:q])
     @cooking_informations = @q.result(distinct: true).includes(:cooking, :fish, :handle)
   end
+
+  def search_time
+    @fishes = Fish.all
+    @cookings = Cooking.all
+  end
 end
