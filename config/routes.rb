3e33dd08cf4c71c9ecb55e800f8aa0e3ba1a6a4a
11090112sS <kosukerino@gmail.com>
2022-03-29 23:10:55 +0900

    Fix ルーティング記述のリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :cooking_informations, only: %i[index] do
-    get 'search_time', on: :collection
-    post 'search_time', on: :collection
-    get 'search_calculate_cooking_time', on: :collection
-    post 'search_calculate_cooking_time', on: :collection
-    get 'calculate_cooking_time', on: :collection
-    post 'calculate_cooking_time', on: :collection
+    collection do
+      get 'search_time'
+      post 'search_time'
+      get 'search_calculate_cooking_time'
+      post 'search_calculate_cooking_time'
+      get 'calculate_cooking_time'
+      post 'calculate_cooking_time'
+    end
   end
 end
