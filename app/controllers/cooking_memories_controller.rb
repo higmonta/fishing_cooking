34ab4b0cd16208b12538883568be0021b3652457
11090112sS <kosukerino@gmail.com>
2022-04-18 03:30:41 +0900

    Add 料理履歴登録機能とエラーメッセージの実装

--- app/controllers/cooking_memories_controller.rb
+++ app/controllers/cooking_memories_controller.rb
@@ -5,6 +5,13 @@ class CookingMemoriesController < ApplicationController
   end
 
   def create
+    @cooking_memory = current_user.cooking_memories.new(cooking_memory_params)
+    if @cooking_memory.save
+      redirect_to cooking_memories_path, success: '登録に成功しました'
+    else
+      flash.now[:danger] = '登録に失敗しました'
+      render :new
+    end
   end
 
   def index
@@ -19,5 +26,11 @@ class CookingMemoriesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def cooking_memory_params
+    params.require(:cooking_memory).permit(:cooking_name, :fish_name, :cooking_date, :cooking_memory_image, :memo)
   end
 end
