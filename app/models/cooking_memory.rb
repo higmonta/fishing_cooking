da10880f1c7e3c852b5720db00c9a44824ca7a7b
11090112sS <kosukerino@gmail.com>
2022-04-17 22:19:56 +0900

    Add CookingMemoryにバリデーションの追加

--- app/models/cooking_memory.rb
+++ app/models/cooking_memory.rb
@@ -1,4 +1,6 @@
 class CookingMemory < ApplicationRecord
   belongs_to :user
   has_one_attached :cooking_memory_image
+  validates :cooking_name, presence: true
+  validates :fish_name, presence: true
 end
