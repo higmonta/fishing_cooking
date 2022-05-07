7bb5657ea89e0e130bb343d9405f4a7e8c89d31c
11090112sS <kosukerino@gmail.com>
2022-05-08 04:12:55 +0900

    Add 料理履歴登録時のデフォルトの料理画像の設定

--- app/models/cooking_memory.rb
+++ app/models/cooking_memory.rb
@@ -1,6 +1,14 @@
 class CookingMemory < ApplicationRecord
+  after_initialize :default_image
+
   belongs_to :user
   has_one_attached :cooking_memory_image
   validates :cooking_name, presence: true
   validates :fish_name, presence: true
+
+  def default_image
+    if !self.cooking_memory_image.attached?
+      self.cooking_memory_image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'cooking_memory', 'default.jpg').to_s), filename: 'default.jpg', content_type: 'image/png/jpg')
+    end
+  end
 end
