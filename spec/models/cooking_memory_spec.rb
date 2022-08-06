5ec9b018a05bc1d47f86a3e195bea6c15a1b6237
11090112sS <kosukerino@gmail.com>
2022-08-07 05:34:05 +0900

    Add CookingMemoryモデルのテストを料理履歴の画像の添付有りの場合も追加

--- spec/models/cooking_memory_spec.rb
+++ spec/models/cooking_memory_spec.rb
@@ -2,24 +2,49 @@ require 'rails_helper'
 
 RSpec.describe CookingMemory, type: :model do
   describe 'バリデーションの検証' do
-    context '全ての属性が存在する場合' do
-      it 'バリデーションエラーにならない' do
-        cooking_memory = build(:cooking_memory)
-        expect(cooking_memory).to be_valid
+    context '画像を添付している時' do
+      context '全ての属性が存在する場合' do
+        it 'バリデーションエラーにならない' do
+          attached_image_cooking_memory = build(:cooking_memory, :attached_image)
+          expect(attached_image_cooking_memory).to be_valid
+        end
+      end
+  
+      context '必須項目の属性が存在しない場合' do
+        it '料理の名前属性が無い場合バリデーションエラーになる' do
+          attached_image_cooking_memory = build(:cooking_memory, :attached_image, cooking_name: nil)
+          attached_image_cooking_memory.valid?
+          expect(attached_image_cooking_memory.errors[:cooking_name]).to include('を入力してください')
+        end
+  
+        it '料理の魚の種類属性が無い場合バリデーションエラーになる' do
+          attached_image_cooking_memory = build(:cooking_memory, :attached_image, fish_name: nil)
+          attached_image_cooking_memory.valid?
+          expect(attached_image_cooking_memory.errors[:fish_name]).to include('を入力してください')
+        end
       end
     end
 
-    context '必須項目の属性が存在しない場合' do
-      it '料理の名前属性が無い場合バリデーションエラーになる' do
-        cooking_memory = build(:cooking_memory, cooking_name: nil)
-        cooking_memory.valid?
-        expect(cooking_memory.errors[:cooking_name]).to include('を入力してください')
+    context '画像を添付していない時' do
+      context '全ての属性が存在する場合' do
+        it 'バリデーションエラーにならない' do
+          not_attached_image_cooking_memory = build(:cooking_memory, :not_attached_image)
+          expect(not_attached_image_cooking_memory).to be_valid
+        end
       end
-
-      it '料理の魚の種類属性がばい場合バリデーションエラーになる' do
-        cooking_memory = build(:cooking_memory, fish_name: nil)
-        cooking_memory.valid?
-        expect(cooking_memory.errors[:fish_name]).to include('を入力してください')
+  
+      context '必須項目の属性が存在しない場合' do
+        it '料理の名前属性が無い場合バリデーションエラーになる' do
+          not_attached_image_cooking_memory = build(:cooking_memory, :not_attached_image, cooking_name: nil)
+          not_attached_image_cooking_memory.valid?
+          expect(not_attached_image_cooking_memory.errors[:cooking_name]).to include('を入力してください')
+        end
+  
+        it '料理の魚の種類属性が無い場合バリデーションエラーになる' do
+          not_attached_image_cooking_memory = build(:cooking_memory, :not_attached_image, fish_name: nil)
+          not_attached_image_cooking_memory.valid?
+          expect(not_attached_image_cooking_memory.errors[:fish_name]).to include('を入力してください')
+        end
       end
     end
   end
