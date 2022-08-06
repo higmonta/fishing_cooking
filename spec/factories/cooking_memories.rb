951c688ec2940f9accd20a9d25d8f0b839e2b1f1
11090112sS <kosukerino@gmail.com>
2022-08-07 05:30:21 +0900

    Add 画像付きの料理履歴のデータを追加

--- spec/factories/cooking_memories.rb
+++ spec/factories/cooking_memories.rb
@@ -1,9 +1,22 @@
 FactoryBot.define do
   factory :cooking_memory do
-    association :user
-    cooking_name{ 'test_cooking_name' }
-    fish_name { 'test_fish_name' }
-    cooking_date { '2022/07/19' }
-    memo { 'test_memo' }
+    trait :attached_image do
+      association :user
+      sequence(:cooking_name) { |n| "test_cooking_name_#{n}" }
+      sequence(:fish_name) { |n| "test_fish_name_#{n}" }
+      cooking_date { '2022/07/19' }
+      sequence(:memo) { |n| "test_memo_#{n}" }
+      after(:build) do |cooking_memory|
+        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
+      end
+    end
+
+    trait :not_attached_image do
+      association :user
+      sequence(:cooking_name) { |n| "test_cooking_name_#{n}" }
+      sequence(:fish_name) { |n| "test_fish_name_#{n}" }
+      cooking_date { '2022/07/19' }
+      sequence(:memo) { |n| "test_memo_#{n}" }
+    end
   end
 end
