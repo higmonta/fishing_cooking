b7716b9e237b154019f4d7eb03707627d181fdf4
11090112sS <kosukerino@gmail.com>
2022-08-07 22:33:59 +0900

    Fix ユーザーのテストデータのパスワードの桁数を10文字以上に変更

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
-    password { '12345aA' }
-    password_confirmation { '12345aA' }
+    password { '123456789aA' }
+    password_confirmation { '123456789aA' }
   end
 end
