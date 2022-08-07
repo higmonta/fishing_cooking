534d65f14a6714169548a7b76595da2a29c61988
11090112sS <kosukerino@gmail.com>
2022-08-07 22:37:13 +0900

    Fix ユーザーのテストデータのパスワードを変更したので、ログインモジュールの入力パスワードを変更

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -2,7 +2,7 @@ module LoginModule
   def login_as(user)
     visit login_path
     fill_in "email", with: user.email
-    fill_in "password", with: "12345aA"
+    fill_in "password", with: "123456789aA"
     click_button "ログイン"
   end
 end
