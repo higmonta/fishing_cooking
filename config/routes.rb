a96db2d85161206933f31b335ebcb4168cba9243
11090112sS <kosukerino@gmail.com>
2022-08-22 02:55:23 +0900

    Fix 不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   resources :cooking_informations, only: %i[index] do
     collection do
       get 'search_time'
-      post 'search_time'
-      get 'search_calculate_cooking_time'
       post 'search_calculate_cooking_time'
-      get 'calculate_cooking_time'
       post 'calculate_cooking_time'
     end
   end
