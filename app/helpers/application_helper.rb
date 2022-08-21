ab06102dd704f077f5c29ef07e248f2eb57e3372
11090112sS <kosukerino@gmail.com>
2022-08-22 01:38:09 +0900

    Add 各ページにタイトルを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def operation_total_time(fish_id:, handle_id:, cooking_information:)
     FishHandleInformation.find_by(fish_id: fish_id, handle_id: handle_id).handle_time + cooking_information.cooking_total_time
   end
+
+  def page_full_title(page_title = '')
+    base_title = 'Fishing Cook'
+    page_title.empty? ? base_title : base_title + ' | ' + page_title
+  end
 end
