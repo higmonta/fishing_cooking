84a83996a9d6e929aa1337cafdb8c9537eeb5ab9
11090112sS <kosukerino@gmail.com>
2022-08-23 01:58:54 +0900

    Fix テストのログインモジュールでログインをクリックする時にヘッダーのログインのリンクと区別しやすいようにスコープの追加

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -3,6 +3,8 @@ module LoginModule
     visit login_path
     fill_in "email", with: user.email
     fill_in "password", with: "123456789aA"
-    click_button "ログイン"
+    within ('.login-button') do
+      click_on 'ログイン'
+    end
   end
 end
