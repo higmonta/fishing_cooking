e0c5021b392e50148c6ac3ecbf5ab0eef172d55d
11090112sS <kosukerino@gmail.com>
2022-09-04 05:12:11 +0900

    Fix CookingMemoryのcooking_date属性を固定値からsequenceに変更

--- spec/factories/cooking_memories.rb
+++ spec/factories/cooking_memories.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
       association :user
       sequence(:cooking_name, 'test_cooking_name_1')
       sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
+      sequence(:cooking_date, '2022/07/19')
       sequence(:memo, 'test_memo_1')
       after(:build) do |cooking_memory|
         cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
@@ -15,7 +15,7 @@ FactoryBot.define do
       association :user
       sequence(:cooking_name, 'test_cooking_name_1')
       sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
+      sequence(:cooking_date, '2022/07/19')
       sequence(:memo, 'test_memo_1')
     end
   end
