8edd3d7636dae22a673d8058ac82d24456b98317
11090112sS <kosukerino@gmail.com>
2022-09-04 04:37:39 +0900

    Fix sequenceの記載方法の変更

--- spec/factories/cooking_memories.rb
+++ spec/factories/cooking_memories.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :cooking_memory do
     trait :attached_image do
       association :user
-      sequence(:cooking_name) { |n| "test_cooking_name_#{n}" }
-      sequence(:fish_name) { |n| "test_fish_name_#{n}" }
+      sequence(:cooking_name, 'test_cooking_name_1')
+      sequence(:fish_name, 'test_fish_name_1')
       cooking_date { '2022/07/19' }
-      sequence(:memo) { |n| "test_memo_#{n}" }
+      sequence(:memo, 'test_memo_1')
       after(:build) do |cooking_memory|
         cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
       end
@@ -13,10 +13,10 @@ FactoryBot.define do
 
     trait :not_attached_image do
       association :user
-      sequence(:cooking_name) { |n| "test_cooking_name_#{n}" }
-      sequence(:fish_name) { |n| "test_fish_name_#{n}" }
+      sequence(:cooking_name, 'test_cooking_name_1')
+      sequence(:fish_name, 'test_fish_name_1')
       cooking_date { '2022/07/19' }
-      sequence(:memo) { |n| "test_memo_#{n}" }
+      sequence(:memo, 'test_memo_1')
     end
   end
 end
