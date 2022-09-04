6df36fda08a97b5808be0b3c5e329776b0a012a5
11090112sS <kosukerino@gmail.com>
2022-09-05 03:12:29 +0900

    Add 料理履歴の検索のシステムテストの追加

--- spec/system/cooking_memories_spec.rb
+++ spec/system/cooking_memories_spec.rb
@@ -392,10 +392,9 @@ RSpec.describe 'CookingMemories', type: :system do
   describe 'ログイン後の料理履歴の削除機能の検証' do
     before { login_as(user) }
 
-    context '画像を添付している時' do
-      let!(:attached_image_cooking_memory) { create(:cooking_memory, :attached_image, user: user) }
-  
+    context '画像を添付している時' do  
       it '料理履歴の削除に成功する' do
+        attached_image_cooking_memory = create(:cooking_memory, :attached_image, user: user)
         visit cooking_memories_path
         click_on '削除'
         expect(page.accept_confirm).to have_content 'こちらの料理履歴を削除してもよろしいですか？'
@@ -409,9 +408,8 @@ RSpec.describe 'CookingMemories', type: :system do
     end
 
     context '画像を添付していない時' do
-      let!(:not_attached_image_cooking_memory) { create(:cooking_memory, :not_attached_image, user: user) }
-  
       it '料理履歴の削除に成功する' do
+        not_attached_image_cooking_memory = create(:cooking_memory, :not_attached_image, user: user)
         visit cooking_memories_path
         click_on '削除'
         expect(page.accept_confirm).to have_content 'こちらの料理履歴を削除してもよろしいですか？'
@@ -421,6 +419,145 @@ RSpec.describe 'CookingMemories', type: :system do
         expect(page).to_not have_content '2022年07月19日'
         expect(page).to_not have_selector("img[src*='default']")
         expect(page).to_not have_content not_attached_image_cooking_memory.memo
+      end
+    end
+  end
+
+  describe 'ログイン後の料理履歴の検索' do
+    before do
+      login_as(user)
+      visit cooking_memories_path
+    end
+
+    context '魚の種類を検索条件に入れて検索した時' do
+      it '検索条件に入れた魚の名前が含まれる料理が検索できる' do
+        cooking_memory_list = create_list(:cooking_memory, 3, :attached_image, user: user)
+        cooking_memory_another_fish_name_list = create_list(:cooking_memory, 3, :attached_image_another_fish_name, user: user)
+        fill_in 'q_fish_name_cont', with: 'test_fish_name'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_list[0].fish_name
+        expect(page).to have_content cooking_memory_list[1].fish_name
+        expect(page).to have_content cooking_memory_list[2].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[0].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[1].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[2].fish_name
+        expect(current_path).to eq cooking_memories_path
+      end
+    end
+
+    context '料理をした日の最初の欄のみを検索条件に入れて検索した時' do
+      it '検索条件に入れた日以降に作成した料理の検索ができる' do
+        cooking_memory_another_cooking_date_1 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/19', user: user)
+        cooking_memory_another_cooking_date_2 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/20', user: user)
+        cooking_memory_another_cooking_date_3 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/21', user: user)
+        fill_in 'q_cooking_date_gteq', with: '002022-07-20'
+        find('.cooking-memories-search-button').click
+        expect(page).to_not have_content cooking_memory_another_cooking_date_1.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to have_content cooking_memory_another_cooking_date_2.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to have_content cooking_memory_another_cooking_date_3.cooking_date.strftime("%Y年%m月%d日")
+        expect(current_path).to eq cooking_memories_path
+      end
+    end
+
+    context '料理をした日の最後の欄のみを検索条件に入れて検索した時' do
+      it '検索条件に入れた日までに作成した料理の検索ができる' do
+        cooking_memory_another_cooking_date_1 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/19', user: user)
+        cooking_memory_another_cooking_date_2 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/20', user: user)
+        cooking_memory_another_cooking_date_3 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/21', user: user)
+        fill_in 'q_cooking_date_lteq', with: '002022-07-20'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_another_cooking_date_1.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to have_content cooking_memory_another_cooking_date_2.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to_not have_content cooking_memory_another_cooking_date_3.cooking_date.strftime("%Y年%m月%d日")
+        expect(current_path).to eq cooking_memories_path
+      end
+    end
+
+    context '料理をした日の最初の欄と最後の欄を検索条件に入れて検索した時' do
+      it '検索条件に入れた期間の間に作成した料理の検索ができる' do
+        cooking_memory_another_cooking_date_1 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/19', user: user)
+        cooking_memory_another_cooking_date_2 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/20', user: user)
+        cooking_memory_another_cooking_date_3 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/21', user: user)
+        fill_in 'q_cooking_date_gteq', with: '002022-07-17'
+        fill_in 'q_cooking_date_lteq', with: '002022-07-20'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_another_cooking_date_1.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to have_content cooking_memory_another_cooking_date_2.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to_not have_content cooking_memory_another_cooking_date_3.cooking_date.strftime("%Y年%m月%d日")
+        expect(current_path).to eq cooking_memories_path
+      end
+    end
+
+    context 'キーワードの欄に検索条件を入れて検索した時' do
+      it 'キーワードに含まれている魚の名前の料理が検索ができる' do
+        cooking_memory_list = create_list(:cooking_memory, 3, :attached_image, user: user)
+        cooking_memory_another_fish_name_list = create_list(:cooking_memory, 3, :attached_image_another_fish_name, user: user)
+        fill_in 'q_fish_name_or_cooking_name_or_memo_cont', with: 'test_fish_name'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_list[0].fish_name
+        expect(page).to have_content cooking_memory_list[1].fish_name
+        expect(page).to have_content cooking_memory_list[2].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[0].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[1].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[2].fish_name
+        expect(current_path).to eq cooking_memories_path
+      end
+
+      it 'キーワードに含まれている料理名の料理が検索できる' do
+        cooking_memory_list = create_list(:cooking_memory, 3, :attached_image, user: user)
+        cooking_memory_another_cooking_name_list = create_list(:cooking_memory, 3, :attached_image_another_cooking_name, user: user)
+        fill_in 'q_fish_name_or_cooking_name_or_memo_cont', with: 'test_cooking_name'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_list[0].cooking_name
+        expect(page).to have_content cooking_memory_list[1].cooking_name
+        expect(page).to have_content cooking_memory_list[2].cooking_name
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[0].cooking_name
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[1].cooking_name
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[2].cooking_name
+        expect(current_path).to eq cooking_memories_path
+      end
+
+      it 'キーワードに含まれているメモの料理が検索できる' do
+        cooking_memory_list = create_list(:cooking_memory, 3, :attached_image, user: user)
+        cooking_memory_another_fish_name_list = create_list(:cooking_memory, 3, :attached_image_another_memo, user: user)
+        fill_in 'q_fish_name_or_cooking_name_or_memo_cont', with: 'test_memo'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_list[0].memo
+        expect(page).to have_content cooking_memory_list[1].memo
+        expect(page).to have_content cooking_memory_list[2].cooking_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[0].memo
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[1].memo
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[2].memo
+        expect(current_path).to eq cooking_memories_path
+      end
+    end
+
+    context '魚の種類の欄と料理をした日の両方の欄とキーワードの欄を検索条件に入れて検索した時' do
+      it '全ての検索条件に当てはまる料理の検索ができる' do
+        cooking_memory_list = create_list(:cooking_memory, 3, :attached_image, user: user)
+        cooking_memory_another_fish_name_list = create_list(:cooking_memory, 3, :attached_image_another_fish_name, user: user)
+        cooking_memory_another_cooking_date_1 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/19', user: user)
+        cooking_memory_another_cooking_date_2 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/20', user: user)
+        cooking_memory_another_cooking_date_3 = create(:cooking_memory, :attached_image, cooking_date: '2022/07/21', user: user)
+        cooking_memory_another_cooking_name_list = create_list(:cooking_memory, 3, :attached_image_another_cooking_name, user: user)
+        fill_in 'q_fish_name_cont', with: 'test_fish_name'
+        fill_in 'q_cooking_date_gteq', with: '002022-07-17'
+        fill_in 'q_cooking_date_lteq', with: '002022-07-20'
+        fill_in 'q_fish_name_or_cooking_name_or_memo_cont', with: 'test_cooking_name'
+        find('.cooking-memories-search-button').click
+        expect(page).to have_content cooking_memory_list[0].cooking_name
+        expect(page).to have_content cooking_memory_list[1].cooking_name
+        expect(page).to have_content cooking_memory_list[2].cooking_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[0].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[1].fish_name
+        expect(page).to_not have_content cooking_memory_another_fish_name_list[2].fish_name
+        expect(page).to have_content cooking_memory_another_cooking_date_1.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to have_content cooking_memory_another_cooking_date_2.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to_not have_content cooking_memory_another_cooking_date_3.cooking_date.strftime("%Y年%m月%d日")
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[0].cooking_name
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[1].cooking_name
+        expect(page).to_not have_content cooking_memory_another_cooking_name_list[2].cooking_name
+        expect(current_path).to eq cooking_memories_path
       end
     end
   end
