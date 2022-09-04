d45174bee84076d5a10b4dfe4a2ce92b1b6cebaa
11090112sS <kosukerino@gmail.com>
2022-09-05 04:32:37 +0900

    Fix Cooking_memoryファクトリーのリファクタリング

--- spec/factories/cooking_memories.rb
+++ spec/factories/cooking_memories.rb
@@ -1,55 +1,27 @@
 FactoryBot.define do
   factory :cooking_memory do
+    association :user
+    cooking_name { 'test_cooking_name' }
+    fish_name { 'test_fish_name' }
+    cooking_date { '2022/07/19' }
+    memo { 'test_memo' }
+  
     trait :attached_image do
-      association :user
-      sequence(:cooking_name, 'test_cooking_name_1')
-      sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
-      sequence(:memo, 'test_memo_1')
       after(:build) do |cooking_memory|
         cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
       end
     end
 
-    trait :attached_image_another_cooking_name do
-      association :user
-      sequence(:cooking_name, 'another_cooking_name_1')
-      sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
-      sequence(:memo, 'test_memo_1')
-      after(:build) do |cooking_memory|
-        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
-      end
+    trait :another_cooking_name do
+      cooking_name { 'another_cooking_name' }
     end
 
-    trait :attached_image_another_fish_name do
-      association :user
-      sequence(:cooking_name, 'test_cooking_name_1')
-      sequence(:fish_name, 'another_fish_name_1')
-      cooking_date { '2022/07/19' }
-      sequence(:memo, 'test_memo_1')
-      after(:build) do |cooking_memory|
-        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
-      end
+    trait :another_fish_name do
+      fish_name { 'another_fish_name' }
     end
 
-    trait :attached_image_another_memo do
-      association :user
-      sequence(:cooking_name, 'test_cooking_name_1')
-      sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
-      sequence(:memo, 'another_memo_1')
-      after(:build) do |cooking_memory|
-        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
-      end
-    end
-
-    trait :not_attached_image do
-      association :user
-      sequence(:cooking_name, 'test_cooking_name_1')
-      sequence(:fish_name, 'test_fish_name_1')
-      cooking_date { '2022/07/19' }
-      sequence(:memo, 'test_memo_1')
+    trait :another_memo do
+      memo { 'another_memo' }
     end
   end
 end
