261d85b296d1316f1a7e547abac48ac93c9a97e6
11090112sS <kosukerino@gmail.com>
2022-09-05 03:11:19 +0900

    Add cooking_memoryのテストデータ作成にtraitの種類を増やした

--- spec/factories/cooking_memories.rb
+++ spec/factories/cooking_memories.rb
@@ -4,8 +4,41 @@ FactoryBot.define do
       association :user
       sequence(:cooking_name, 'test_cooking_name_1')
       sequence(:fish_name, 'test_fish_name_1')
-      sequence(:cooking_date, '2022/07/19')
+      cooking_date { '2022/07/19' }
       sequence(:memo, 'test_memo_1')
+      after(:build) do |cooking_memory|
+        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
+      end
+    end
+
+    trait :attached_image_another_cooking_name do
+      association :user
+      sequence(:cooking_name, 'another_cooking_name_1')
+      sequence(:fish_name, 'test_fish_name_1')
+      cooking_date { '2022/07/19' }
+      sequence(:memo, 'test_memo_1')
+      after(:build) do |cooking_memory|
+        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
+      end
+    end
+
+    trait :attached_image_another_fish_name do
+      association :user
+      sequence(:cooking_name, 'test_cooking_name_1')
+      sequence(:fish_name, 'another_fish_name_1')
+      cooking_date { '2022/07/19' }
+      sequence(:memo, 'test_memo_1')
+      after(:build) do |cooking_memory|
+        cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
+      end
+    end
+
+    trait :attached_image_another_memo do
+      association :user
+      sequence(:cooking_name, 'test_cooking_name_1')
+      sequence(:fish_name, 'test_fish_name_1')
+      cooking_date { '2022/07/19' }
+      sequence(:memo, 'another_memo_1')
       after(:build) do |cooking_memory|
         cooking_memory.cooking_memory_image.attach(io: File.open("#{Rails.root}/spec/fixtures/images/test1_image.jpg"), filename: 'test1_image.jpg', content_type: 'image/png/jpg')
       end
@@ -15,7 +48,7 @@ FactoryBot.define do
       association :user
       sequence(:cooking_name, 'test_cooking_name_1')
       sequence(:fish_name, 'test_fish_name_1')
-      sequence(:cooking_date, '2022/07/19')
+      cooking_date { '2022/07/19' }
       sequence(:memo, 'test_memo_1')
     end
   end
