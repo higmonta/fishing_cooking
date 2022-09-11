b0120bd6d2589c04b969ec4362285ee5c3b79731
11090112sS <kosukerino@gmail.com>
2022-09-12 06:06:44 +0900

    Fix 料理時間計算の計算式が誤っていたので訂正

--- app/forms/calculate_cooking_time.rb
+++ app/forms/calculate_cooking_time.rb
@@ -22,24 +22,34 @@ class CalculateCookingTime
     cooking_total_time = cooking_information.cooking_total_time  #T:魚を捌いてある状態から料理して何分で完成するか(T=t1+t2)
     rest_fish_time = cooking_information.rest_fish_time   # t1:料理する際に魚を何分寝かせるか
     cooking_time = cooking_information.cooking_time    #  t2:魚を何分焼いたり揚げたりするか
-    if rest_fish_time <= cooking_time
-      if cookware_capacity <= let_foodstuff_capacity
-        ((count/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
-      else
-        ((count/let_foodstuff_capacity).ceil - 1) * cooking_time + cooking_total_time
-      end
-    else
-      if cookware_capacity < let_foodstuff_capacity
-        if count <= let_foodstuff_capacity
-          ((let_foodstuff_capacity/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
+    if !rest_fish_time.zero? && !cooking_time.zero?
+      if rest_fish_time <= cooking_time
+        if cookware_capacity <= let_foodstuff_capacity
+          ((count/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
         else
-          if ((let_foodstuff_capacity/cookware_capacity).ceil) * cooking_time >= rest_fish_time
-            ((let_foodstuff_capacity/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time + ((count - let_foodstuff_capacity)/cookware_capacity).ceil * cooking_total_time
-          else
-            ((let_foodstuff_capacity/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time + ((count - let_foodstuff_capacity)/cookware_capacity).ceil * (rest_fish_time - (let_foodstuff_capacity/cookware_capacity).ceil * cooking_time + cooking_time)
-          end
+          ((count/let_foodstuff_capacity).ceil - 1) * cooking_time + cooking_total_time
         end
       else
+        if cookware_capacity < let_foodstuff_capacity
+          if count <= let_foodstuff_capacity
+            ((count/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
+          else
+            if ((let_foodstuff_capacity/cookware_capacity).ceil) * cooking_time >= rest_fish_time
+              ((let_foodstuff_capacity/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
+            else
+              ((let_foodstuff_capacity/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time + ((count - let_foodstuff_capacity)/let_foodstuff_capacity).ceil * rest_fish_time
+            end
+          end
+        else
+          ((count/let_foodstuff_capacity).ceil - 1) * rest_fish_time + cooking_total_time
+        end
+      end
+    else
+      if rest_fish_time.zero? && cooking_time.zero?
+        0
+      elsif rest_fish_time.zero? && !cooking_time.zero?
+        ((count/cookware_capacity).ceil - 1) * cooking_time + cooking_total_time
+      elsif !rest_fish_time.zero? && cooking_time.zero?
         ((count/let_foodstuff_capacity).ceil - 1) * rest_fish_time + cooking_total_time
       end
     end
