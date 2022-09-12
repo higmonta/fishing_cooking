1f53af8d87ffee42c4da9c0b9a685a558d27a5fc
11090112sS <kosukerino@gmail.com>
2022-09-13 04:27:58 +0900

    Add CookwareモデルとCookingInformationモデルのアソシエーションの設定

--- app/models/cooking_information.rb
+++ app/models/cooking_information.rb
@@ -2,6 +2,7 @@ class CookingInformation < ApplicationRecord
   belongs_to :cooking
   belongs_to :fish
   belongs_to :handle
+  belongs_to :cookware
   validates :cooking_id, uniqueness: { scope: %i[fish_id handle_id] }
   validates :cooking_id, uniqueness: { scope: :fish_id }
   validates :cooking_time, presence: true
--- app/models/cookware.rb
+++ app/models/cookware.rb
@@ -1,2 +1,4 @@
 class Cookware < ApplicationRecord
+  has_many :cooking_informations
+  validates :name, uniqueness: true, presence: true
 end
