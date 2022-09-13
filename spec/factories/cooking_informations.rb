6d5972d088935af3c79311b2485c099c7b7b5c9d
11090112sS <kosukerino@gmail.com>
2022-09-14 03:39:18 +0900

    Add Cookwareモデルを追加したので、factoryにもcookwareを追加

--- spec/factories/cooking_informations.rb
+++ spec/factories/cooking_informations.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     association :cooking
     association :fish
     association :handle
+    association :cookware
     cooking_time { rand(100) }
     cooking_url { Faker::Internet.url }
     rest_fish_time { rand(100) }
---
+++ spec/factories/cookwares.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :cookware do
+    sequence(:name, 'cookware_1')
+  end
+end
