b6e1041079bcf7cf79a0fdf70ca25689cc29f71a
11090112sS <kosukerino@gmail.com>
2022-09-28 01:42:46 +0900

    Fix タイトルの出力が誤っていたので訂正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,6 @@ module ApplicationHelper
 
   def page_full_title(page_title = '')
     base_title = 'Fishing Cook'
-    page_title.empty? ? base_title : "#{base_title} + ' | ' + #{page_title}"
+    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
   end
 end
