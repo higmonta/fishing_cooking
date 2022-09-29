fa4505509850dd4e4d9f5e79ed30f20b89cf4476
11090112sS <kosukerino@gmail.com>
2022-09-29 11:23:38 +0900

    Add SEO対策で、herokuapp.comでインデクシングされないように独自ドメインにリダイレクトしてドメインを統一

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_domain
+  FQDN = 'www.fishing-cook.com'
+
   private
 
   def not_authenticated
     flash[:danger] = t 'defaults.login_required'
     redirect_to login_path
+  end
+
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    # 主にlocalテスト用の対策で80と443以外でアクセスされた場合ポート番号をURLに含める
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}"
   end
 end
