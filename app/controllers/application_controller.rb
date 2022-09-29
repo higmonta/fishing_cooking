e610057a3ece8d78df770a1ba0d79975548cf259
11090112sS <kosukerino@gmail.com>
2022-09-29 11:53:42 +0900

    Fix Lintの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :ensure_domain
-  FQDN = 'www.fishing-cook.com'
+  FQDN = 'www.fishing-cook.com'.freeze
 
   private
 
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
   def ensure_domain
     return unless /\.herokuapp.com/ =~ request.host
+
     # 主にlocalテスト用の対策で80と443以外でアクセスされた場合ポート番号をURLに含める
     port = ":#{request.port}" unless [80, 443].include?(request.port)
     redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}"
